05cc8484b4303261ef4784ad1362b08592936bb2
Hyo Young <gydud7300@naver.com>
2023-08-05 21:45:50 +0900

    키오스크 퀴즈

--- front/static/caffe_order/caffe_quiz_1920/kiosk_quiz.js
+++ front/static/caffe_order/caffe_quiz_1920/kiosk_quiz.js
@@ -4,6 +4,11 @@ function page_default(){
     document.getElementById("explain_box_3").style.display = 'none';
     document.getElementById("explain_box_4").style.display = 'none';
     document.getElementById("explain_box_5").style.display = 'none';
+    document.getElementById("step_1").style.display = 'none';
+    document.getElementById("step_2").style.display = 'none';
+    document.getElementById("step_3").style.display = 'none';
+    document.getElementById("step_4").style.display = 'none';
+    document.getElementById("step_5").style.display = 'none';
 
     document.getElementById("dis_step").style.display = 'none';
     document.getElementById("dis_start").style.display = 'flex';
@@ -17,26 +22,36 @@ function dis_start_fade(){
 function dis_step_fade(){
     document.getElementById("dis_step").style.display = 'none';
     document.getElementById("explain_box_1").style.display = 'flex';
+    document.getElementById("step_1").style.display = 'flex';
 }
 
 
 function next_text_1(){
     document.getElementById("explain_box_1").style.display = 'none';
+    document.getElementById("step_1").style.display = 'none';
     document.getElementById("explain_box_2").style.display = 'flex';
+    document.getElementById("step_2").style.display = 'flex';
 }
 function next_text_2(){
     document.getElementById("explain_box_2").style.display = 'none';
+    document.getElementById("step_2").style.display = 'none';
     document.getElementById("explain_box_3").style.display = 'flex';
+    document.getElementById("step_3").style.display = 'flex';
 }
 function next_text_3(){
     document.getElementById("explain_box_3").style.display = 'none';
+    document.getElementById("step_3").style.display = 'none';
     document.getElementById("explain_box_4").style.display = 'flex';
+    document.getElementById("step_4").style.display = 'flex';
 }
 function next_text_4(){
     document.getElementById("explain_box_4").style.display = 'none';
+    document.getElementById("step_4").style.display = 'none';
     document.getElementById("explain_box_5").style.display = 'flex';
+    document.getElementById("step_5").style.display = 'flex';
 }
 function replay(){
     document.getElementById("explain_box_5").style.display = 'none';
+    document.getElementById("step_5").style.display = 'none';
     document.getElementById("dis_start").style.display = 'flex';
 }
