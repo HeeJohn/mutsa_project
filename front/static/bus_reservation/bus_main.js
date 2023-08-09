5cf8216edd312f0658f8ebb7034cd18d003e13da
Hyo Young <gydud7300@naver.com>
2023-08-10 00:48:59 +0900

    버스 시뮬 레이아웃 아직 미완성... why? 중간에 날아감...그래서 다시 첨부터 많듬... 그리고 알 수 없는 두통...

---
+++ front/static/bus_reservation/bus_main.js
@@ -0,0 +1,39 @@
+function page_default(){
+    document.getElementById("page_1").style.display = "flex";
+    document.getElementById("page_2").style.display = "none";
+    document.getElementById("page_3").style.display = "none";
+    document.getElementById("page_4").style.display = "none";
+    document.getElementById("page_5").style.display = "none";
+}
+
+function dis_page_2(){
+    document.getElementById("page_1").style.display = "none";
+    document.getElementById("page_2").style.display = "flex";
+    document.getElementById("page_3").style.display = "none";
+    document.getElementById("page_4").style.display = "none";
+    document.getElementById("page_5").style.display = "none";
+}
+
+function dis_page_3(){
+    document.getElementById("page_1").style.display = "none";
+    document.getElementById("page_2").style.display = "none";
+    document.getElementById("page_3").style.display = "flex";
+    document.getElementById("page_4").style.display = "none";
+    document.getElementById("page_5").style.display = "none";
+}
+
+function dis_page_4(){
+    document.getElementById("page_1").style.display = "none";
+    document.getElementById("page_2").style.display = "none";
+    document.getElementById("page_3").style.display = "none";
+    document.getElementById("page_4").style.display = "flex";
+    document.getElementById("page_5").style.display = "none";
+}
+
+function dis_page_5(){
+    document.getElementById("page_1").style.display = "none";
+    document.getElementById("page_2").style.display = "none";
+    document.getElementById("page_3").style.display = "none";
+    document.getElementById("page_4").style.display = "none";
+    document.getElementById("page_5").style.display = "flex";
+}
