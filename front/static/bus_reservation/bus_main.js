c319f4d33a29ae4611522b92305a8bcdffd2294f
Hyo Young <gydud7300@naver.com>
2023-08-11 13:04:59 +0900

    버스 시뮬 시계추가

--- front/static/bus_reservation/bus_main.js
+++ front/static/bus_reservation/bus_main.js
@@ -1,12 +1,37 @@
-function page_default(){
+function displayClock(targetId) {  //시계함수 - 아이디만 넣으면 동작가능
+    let target = document.getElementById(targetId);
+
+    function updateClock() {
+        let time = new Date();
+
+        let month = time.getMonth();
+        let date = time.getDate();
+        let day = time.getDay();
+        let week = ['일', '월', '화', '수', '목', '금', '토'];
+
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+
+        target.innerText = 
+        `${month + 1}월 ${date}일 (${week[day]}) ` +
+        `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    }
+
+    updateClock();
+    setInterval(updateClock, 1000);
+}
+
+window.onload=function page_default() { /*페이지 로드시 동작하는 함수*/
     document.getElementById("page_1").style.display = "flex";
     document.getElementById("page_2").style.display = "none";
     document.getElementById("page_3").style.display = "none";
     document.getElementById("page_4").style.display = "none";
     document.getElementById("page_5").style.display = "none";
+    displayClock("page_1_time");
 }
 
-function dis_page_2(){
+function dis_page_2() {
     document.getElementById("page_1").style.display = "none";
     document.getElementById("page_2").style.display = "flex";
     document.getElementById("page_3").style.display = "none";
@@ -14,15 +39,16 @@ function dis_page_2(){
     document.getElementById("page_5").style.display = "none";
 }
 
-function dis_page_3(){
+function dis_page_3() {
     document.getElementById("page_1").style.display = "none";
     document.getElementById("page_2").style.display = "none";
     document.getElementById("page_3").style.display = "flex";
     document.getElementById("page_4").style.display = "none";
     document.getElementById("page_5").style.display = "none";
+    displayClock("page_3_date_section");
 }
 
-function dis_page_4(){
+function dis_page_4() {
     document.getElementById("page_1").style.display = "none";
     document.getElementById("page_2").style.display = "none";
     document.getElementById("page_3").style.display = "none";
@@ -30,7 +56,7 @@ function dis_page_4(){
     document.getElementById("page_5").style.display = "none";
 }
 
-function dis_page_5(){
+function dis_page_5() {
     document.getElementById("page_1").style.display = "none";
     document.getElementById("page_2").style.display = "none";
     document.getElementById("page_3").style.display = "none";
