0b8671327da31bbb5c0eb8928a3cb4d4c0105ef1
maintains00 <wltjs2587@gmail.com>
2023-08-17 20:59:01 +0900

    김도헌 바보

--- web/settings.py
+++ web/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'kiosk',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
  
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
